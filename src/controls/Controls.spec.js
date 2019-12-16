945d5880fb3394962a2e0f8ace85655074c3faa8
spencer-mcguire <smcguire1826@gmail.com>
2019-12-16 16:11:55 -0600

    test buttons and state changes

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, getByTitle, fireEvent } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Controls from "./Controls";
 
 test("Controls render properly", () => {
@@ -8,9 +8,27 @@ test("Controls render properly", () => {
 });
 
 test("Unlock Gate", () => {
-  let locked = true;
-  let closed = false;
-  const { getByTestId } = render(<Controls locked={locked} closed={closed} />);
+  const { getByTestId } = render(<Controls locked={true} closed={false} />);
 
   getByTestId("mainControls");
+});
+
+test("Provide buttons to toggle closed and lock states", () => {
+  const { getByText } = render(<Controls closed={false} locked={false} />);
+  getByText(/lock gate/i);
+  getByText(/close gate/i);
+});
+
+test("Close gate changes state to closed if clicked", () => {
+  const { getByText, findByText } = render(<Controls closed={false} />);
+  const button = getByText(/close gate/i);
+  fireEvent.click(button);
+  findByText(/open gate /i);
+});
+
+test("Close gate changes state to closed if clicked", () => {
+  const { getByText, findByText } = render(<Controls locked={false} />);
+  const button = getByText(/lock gate/i);
+  fireEvent.click(button);
+  findByText(/unlock gate /i);
 });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -5,3 +5,8 @@ import Dashboard from "./Dashboard";
 test("FULL dashboard renders without crashing", () => {
   render(<Dashboard />);
 });
+
+test("Matches Snapshot", () => {
+  const { baseElement } = render(<Dashboard />);
+  expect(baseElement).toMatchSnapshot();
+});

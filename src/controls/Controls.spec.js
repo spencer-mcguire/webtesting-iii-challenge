00c04044bdad2ca20756d79ecfaade6da166b660
spencer-mcguire <smcguire1826@gmail.com>
2019-12-16 13:56:06 -0600

    test if controls renders

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,8 @@
-// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Controls from "./Controls";
+
+test("Controls render properly", () => {
+  const { getByTestId } = render(<Controls />);
+  getByTestId("mainControls");
+});

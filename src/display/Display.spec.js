9928a97f718e89f7a6cf2076acae6211c1b224fb
spencer-mcguire <smcguire1826@gmail.com>
2019-12-16 13:50:22 -0600

    test if display renders

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,8 @@
-// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Display from "./Display";
+
+test("Display renders properly", () => {
+  const { getByTestId } = render(<Display closed locked />);
+  getByTestId("mainDisplay");
+});
